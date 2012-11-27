d527f40603bc8bf8fbbe54c910d168988f77c2f0
Neel Patel <npatel@college.harvard.edu>
2012-11-27 01:52:20 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cs50::Application.config.secret_token = '10e8b3fcccbf366d88a85b71bdba0e693381f66067c408d53f9e6ca0bd85115983f15405bb4501c81394a1157bb3153649c34dd80aad4e7205b69840a87b3ad4'
